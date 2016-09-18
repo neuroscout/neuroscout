127239a1337a8fe27059901eb7d9b49dac0ac219
Alejandro <delavega@colorado.edu>
2016-09-18 17:14:20 -0500

    Restructured and created init migrations folder

--- web/config.py
+++ web/config.py
@@ -3,12 +3,16 @@ import os
 class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'enter key here'
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "??"
+    SECRET_KEY = os.environ['SECRET_KEY']
+    DEBUG = os.environ['DEBUG']
+    DB_NAME = os.environ['DB_NAME']
+    DB_USER = os.environ['DB_USER']
+    DB_PASS = os.environ['DB_PASS']
+    DB_SERVICE = os.environ['DB_SERVICE']
+    DB_PORT = os.environ['DB_PORT']
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
+        DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
+    )
 
 class DevelopmentConfig(Config):
     DEBUG = True
-
-    SQLALCHEMY_DATABASE_URI = "??"
--- web/manage.py
+++ web/manage.py
@@ -1,11 +1,11 @@
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from app import app, db
 import os
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-migrate = Migrate(app, db, compare=True)
+migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
