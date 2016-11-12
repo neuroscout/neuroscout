6b274461f674b0482af195cf1b3d85dc7d6e76f5
Alejandro <delavega@colorado.edu>
2016-11-11 19:48:17 -0600

    added db utils:

---
+++ web/db_utils.py
@@ -0,0 +1,12 @@
+def copy_row(model, row, ignored_columns=[]):
+    copy = model()
+
+    for col in row.__table__.columns:
+        if col.name not in ignored_columns:
+            try:
+                copy.__setattr__(col.name, getattr(row, col.name))
+            except Exception as e:
+                print(e)
+                continue
+
+    return copy
