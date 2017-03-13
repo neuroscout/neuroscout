e99d6ea12566a0f24898a8a6cfeb0221b4f8a4e7
adelavega <delavega@utexas.edu>
2017-03-13 18:28:09 -0500

    Updated resources to match models

---
+++ web/resources/run.py
@@ -0,0 +1,35 @@
+from flask_restful import Resource, abort
+from flask_restful_swagger.swagger import operation
+from flask_jwt import jwt_required
+from marshmallow import Schema, fields, post_load, validates, ValidationError
+from models.run import Run
+
+class RunSchema(Schema):
+	id = fields.Str(dump_only=True)
+
+	# Could potentially include ExtractedEvents as a nested Schema
+	@post_load
+	def make_db(self, data):
+		return Run(**data)
+
+	class Meta:
+		additional = ('session', 'subject', 'number', 'task')
+
+class RunResource(Resource):
+	""" Resource for Run """
+	@operation(
+	responseMessages=[
+	    {
+	      "code": 400,
+	      "message": "Run doesn't exist"
+	    },
+	  ]
+	)
+	@jwt_required()
+	def get(self, run_id):
+		""" Access a run """
+		result = Run.query.filter_by(id=run_id).one()
+		if result:
+			return RunSchema().dump(result)
+		else:
+			abort(400, message="Run {} doesn't exist".format(stimulus_id))
