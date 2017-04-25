1e9e8bb474c354bfc90a4abd88e13a09c655d572
adelavega <delavega@utexas.edu>
2017-04-25 13:55:46 -0500

    Minor tweaks

--- web/resources/dataset.py
+++ web/resources/dataset.py
@@ -1,6 +1,5 @@
 from flask_restful import Resource
 from flask_restful_swagger.swagger import operation
-from flask_jwt import jwt_required
 from marshmallow import Schema, fields, post_load
 
 from models.dataset import Dataset
@@ -23,7 +22,6 @@ class DatasetResource(Resource):
 	@operation(
 	responseMessages=[{"code": 404,
 	      "message": "Dataset does not exist"}])
-	@jwt_required()
 	def get(self, dataset_id):
 		""" Access a specific dataset """
 		result = Dataset.query.filter_by(id=dataset_id).first_or_404()
@@ -32,7 +30,6 @@ class DatasetResource(Resource):
 class DatasetListResource(Resource):
 	""" Available datasets """
 	@operation()
-	@jwt_required()
 	def get(self):
 		""" List of datasets """
 		result = Dataset.query.all()
