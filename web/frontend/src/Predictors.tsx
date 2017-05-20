7e9671d0627e9bf217e8d326c195d7496456f85a
Pouya Razavi <pouya@pouya.net>
2017-05-20 16:24:33 -0500

    WIP on frontend: implemented search by name & description for predictors

--- web/frontend/src/Predictors.tsx
+++ web/frontend/src/Predictors.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { Table } from 'antd'
-import { TableProps, TableRowSelection } from "antd/lib/table/Table";
+import { Table, Input, Button } from 'antd';
+import { TableProps, TableRowSelection } from 'antd/lib/table/Table';
 import { Analysis, Predictor } from './commontypes';
 
-
-class PredictorTable extends React.Component<TableProps<any>, any>{
+class PredictorTable extends React.Component<TableProps<any>, any> {
   render() {
-    return <Table {...this.props} />
+    return <Table {...this.props} />;
   }
 }
 
@@ -16,11 +15,33 @@ interface PredictorTabProps {
   updateAnalysis: (value: any) => void;
 }
 
-export class PredictorsTab extends React.Component<PredictorTabProps, any>{
+interface PredictorsTabState {
+  searchText: string;
+  filteredPredictors: Predictor[];
+}
+
+export class PredictorsTab extends React.Component<PredictorTabProps, PredictorsTabState> {
+  state = {
+    searchText: '',
+    filteredPredictors: this.props.availablePredictors
+  };
+
+  onInputChange = (e) => {
+    const { availablePredictors } = this.props;
+    const searchText: string = e.target.value;
+    const searchRegex = new RegExp(searchText.trim(), 'i');
+    const newState: PredictorsTabState = { searchText, filteredPredictors: availablePredictors };
+    if (searchText.length > 2) {
+      newState.filteredPredictors = availablePredictors.filter(
+        p => searchRegex.test(p.name + (p.description || '')));
+    }
+    this.setState(newState);
+  }
+
   updateAnalysis = (attrName: keyof Analysis) => (value: any) => {
     let newAnalysis = { ...this.props.analysis };
     newAnalysis[attrName] = value;
-    this.props.updateAnalysis(newAnalysis)
+    this.props.updateAnalysis(newAnalysis);
   }
 
   updateAnalysisFromEvent = (attrName: keyof Analysis) => (event: any) => {
@@ -29,6 +50,7 @@ export class PredictorsTab extends React.Component<PredictorTabProps, any>{
 
   render() {
     const { availablePredictors } = this.props;
+    const { filteredPredictors } = this.state;
     const columns = [
       { title: 'ID', dataIndex: 'id' },
       { title: 'Name', dataIndex: 'name' },
@@ -36,25 +58,35 @@ export class PredictorsTab extends React.Component<PredictorTabProps, any>{
     ];
 
     const rowSelection: TableRowSelection<Predictor> = {
-      onSelect: (record, selected, selectedRows: any) => {
-        console.log('Selected rows = ', selectedRows);
-        this.updateAnalysis('predictorIds')(selectedRows.map(x => x.id))
+      onSelect: (record, selected, selectedRows: Predictor[]) => {
+        this.updateAnalysis('predictorIds')(selectedRows.map(x => x.id));
       },
-      onSelectAll: (selected, selectedRows: any, changeRows) => {
-        this.updateAnalysis('predictorIds')(selectedRows.map(x => x.id))
+      onSelectAll: (selected, selectedRows: Predictor[], changeRows) => {
+        this.updateAnalysis('predictorIds')(selectedRows.map(x => x.id));
       }
-    }
+    };
 
-    return <div>
+    return (
       <div>
-        <p>Select predictors:</p>
-        <PredictorTable
-          columns={columns}
-          rowKey="id"
-          pagination={false}
-          dataSource={availablePredictors}
-          rowSelection={rowSelection} />
+        <div>
+          <Input
+            placeholder="Search predictor name or description..."
+            value={this.state.searchText}
+            onChange={this.onInputChange}
+          /><br /><br />
+        </div>
+        <div>
+          <p>{`Select predictors (displaying ${filteredPredictors.length} 
+            out of ${availablePredictors.length} total predictors):`}</p>
+          <PredictorTable
+            columns={columns}
+            rowKey="id"
+            pagination={false}
+            size="small"
+            dataSource={this.state.filteredPredictors}
+            rowSelection={rowSelection} />
+        </div>
       </div>
-    </div>
+    );
   }
 }
