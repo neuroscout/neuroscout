67ff85ae7423c0576296841dc461ba26709d3b2a
adelavega <delavega@utexas.edu>
2017-05-21 17:43:07 -0500

    Imageio downloaded when needed, easier to rebuid web

--- celery/app.py
+++
@@ -1,5 +0,0 @@
-from celery import Celery
-from os import environ
-
-celery_app = Celery('tasks', broker=environ.get('CELERY_BROKER_URL'),
-             backend=environ.get('CELERY_RESULT_BACKEND'), include=['tasks'])
--- celery/tasks.py
+++
@@ -1,7 +0,0 @@
-import time
-from app import celery_app
-
-@celery_app.task(name='mytasks.add')
-def add(x, y):
-    time.sleep(5) # lets sleep for a while before doing the gigantic addition task!
-    return x + y
