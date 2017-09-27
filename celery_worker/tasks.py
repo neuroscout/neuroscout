d6e6a8a69028bddaa59b3f6a4c8754c92e06dc17
adelavega <delavega@utexas.edu>
2017-09-27 18:51:31 -0500

    Using cli package

--- celery_worker/tasks.py
+++ celery_worker/tasks.py
@@ -1,11 +1,7 @@
 from app import celery_app
 from celery.utils.log import get_task_logger
 
-import sys
-sys.path.insert(1, '/neuroscout/workflow/')
-from importlib import import_module
-fmri = import_module('fmri_bids_firstlevel')
-
+from neuroscout_cli.workflows import fmri_firstlevel
 logger = get_task_logger(__name__)
 
 @celery_app.task(name='workflow.compile')
@@ -16,4 +12,4 @@ def validate(analysis_json, bids_path):
     args.update({'-b' : bids_path, '<bundle>': analysis_json, '--jobs' : 1,
                  '--disable-datalad' : True})
 
-    fmri.FirstLevel(args)
+    fmri_firstlevel.FirstLevel(args)
