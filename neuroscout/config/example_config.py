833dbca48a6444f29071a0de07e1ac6b7b67b219
adelavega <delavega@utexas.edu>
2017-10-14 14:30:41 -0500

    Removed config. Use example config

--- neuroscout/config/example_config.py
+++ neuroscout/config/example_config.py
@@ -1,12 +1,52 @@
+import datetime
+
 class Config(object):
     DEBUG = False
+    TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'enter key here'
+    SECRET_KEY = 'A_SECRET!'
+    SECURITY_PASSWORD_HASH = 'dfdfd'
+    SECURITY_PASSWORD_SALT = 'A_SECRET'
+    WTF_CSRF_ENABLED = False
+    JWT_EXPIRATION_DELTA = datetime.timedelta(days=7)
+    JWT_AUTH_URL_RULE = '/api/auth'
+    JWT_AUTH_USERNAME_KEY = 'email'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    HASH_SALT = 'dfdfdf'
+    MIGRATIONS_DIR = 'A_SECRET'
+    APISPEC_SWAGGER_UI_URL = None
 
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "??"
+    MAIL_SERVER = ''
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
+    MAIL_DEFAULT_SENDER = 'no-reply@neuroscout.org'
+    SECURITY_EMAIL_SENDER = 'no-reply@neuroscout.org'
+
+    CONFIRM_USERS = True
+    SEND_REGISTER_EMAIL = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres@postgres:5432/neuroscout'
+    PROPAGATE_EXCEPTIONS = True
+    MIGRATIONS_DIR = '/migrations/migrations'
+    DATASET_DIR = '/file-data'
 
-    SQLALCHEMY_DATABASE_URI = "??"
+class TestingConfig(Config):
+    TESTING = True
+
+class DockerTestConfig(TestingConfig):
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres@postgres:5432/scout_test'
+
+class HomeTestingConfig(TestingConfig):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://zorro:dbpass@localhost:5432/scout_test'
+    DATASET_DIR = '/tmp/file-data'
+
+class TravisConfig(TestingConfig):
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost/travis_ci_test"
+    DATASET_DIR = '/tmp/file-data'
+
+class HomeConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://zorro:dbpass@localhost:5432/neuroscout'
+    PROPAGATE_EXCEPTIONS = True
+    DEBUG = True
