51ec71e71f1ac63fa33740ed7ecfc3a65f650162
adelavega <delavega@utexas.edu>
2018-06-06 15:27:01 -0500

    Speed up dataset

--- neuroscout/resources/dataset.py
+++ neuroscout/resources/dataset.py
@@ -10,8 +10,6 @@ class DatasetSchema(Schema):
 	description = fields.Dict()
 	mimetypes = fields.List(fields.Str(),
                          description='Dataset mimetypes/modalities')
-	tasks = fields.List(fields.Str(),
-                     description='Tasks in dataset runs.')
 	runs = fields.Nested('RunSchema', many=True, only=['id'])
 	tasks = fields.Nested('TaskSchema', many=True, only=['id', 'name'])
 	dataset_address = fields.Str(description='BIDS Dataset remote address')
@@ -25,6 +23,7 @@ class DatasetResource(MethodResource):
 
 class DatasetListResource(MethodResource):
     @doc(tags=['dataset'], summary='Returns list of datasets.')
-    @marshal_with(DatasetSchema(many=True))
+    @marshal_with(DatasetSchema(many=True,
+								exclude=['mimetypes', 'dataset_address', 'preproc_address']))
     def get(self):
     	return Dataset.query.all()
