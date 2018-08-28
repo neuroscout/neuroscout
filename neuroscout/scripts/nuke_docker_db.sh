b5bdd06fe25552c50fede53174581a292bf7a312
adelavega <delavega@utexas.edu>
2018-08-28 13:16:15 -0500

    Remove unused scripts directory

--- neuroscout/scripts/nuke_docker_db.sh
+++
@@ -1,3 +0,0 @@
-docker-compose exec postgres psql -h postgres -U postgres -c "drop database neuroscout"
-docker-compose exec postgres psql -h postgres -U postgres -c "create database neuroscout"
-docker-compose exec neuroscout bash scripts/init_reset.sh
