2004a86922b8f50b65239b00d5ea2d29bfe932f2
adelavega <delavega@utexas.edu>
2018-09-20 16:04:32 -0500

    Refactored celery to split up reports and compilation

--- celery_worker/tasks.py
+++ celery_worker/tasks.py
@@ -9,33 +9,43 @@ from bids.analysis import Analysis
 from bids.layout import BIDSLayout
 from grabbit import Layout
 from copy import deepcopy
+from celery.contrib import rdb
+
+from collections import defaultdict
 
 logger = get_task_logger(__name__)
 PATHS = ['sub-{subject}_[ses-{session}_]task-{task}_[acq-{acquisition}_][run-{run}_]events.tsv']
 
-def writeout_events(analysis, pes, outdir):
-    """ Write event files from JSON """
+def _get_entities(run):
+    """ Get BIDS-entities from run object """
+    entities = {
+        r:v
+        for r,v in run.items()
+        if r in ['number', 'session', 'subject', 'acquisition'] and v is not None
+        }
+
+    entities['run'] = entities.pop('number')
+    return entities
+
+def _writeout_events(analysis, pes, outdir):
+    """ Writeout predictor_events into BIDS event files """
     gl = Layout(outdir.as_posix())
     outdir = outdir / "func"
     outdir.mkdir(exist_ok=True)
 
-    desc = {"Name": "Events", "BIDSVersion": "1.0"}
+    desc = {'Name': 'Events', 'BIDSVersion': '1.0'}
     json.dump(desc, (outdir / 'dataset_description.json').open('w'))
-
     # Load events and rename columns to human-readable
     pes = pd.DataFrame(pes)
-    predictor_names = {di["id"]: di["name"] for di in analysis['predictors']}
+    predictor_names = {p['id']:p['name'] for p in analysis['predictors']}
     pes.predictor_id = pes.predictor_id.map(predictor_names)
 
     # Write out event files
     paths = []
-    for run in analysis.pop('runs'):
+    for run in analysis.get('runs'):
         # Write out event files for each run_id
         run_events = pes[pes.run_id==run['id']].drop('run_id', axis=1)
-
-        run['run'] = run.pop('number')
-        entities = {r:v for r,v in run.items()
-                    if r in ['run', 'session', 'subject', 'acquisition'] and v is not None}
+        entities = _get_entities(run)
         entities['task'] = analysis['task_name']
 
         if run_events.empty is False:
@@ -51,32 +61,60 @@ def writeout_events(analysis, pes, outdir):
 
     return paths
 
-@celery_app.task(name='workflow.compile')
-def compile(analysis, predictor_events, resources, bids_dir):
-    files_dir = Path(mkdtemp())
-    model = analysis.pop('model')
+def _merge_dictionaries(*arg):
+    """ Set merge dictionaries """
+    dd = defaultdict(set)
 
-    kwargs = {'scan_length': analysis['runs'][0]['duration'],
-              'subject': [model['input']['subject'][0]],
-              'task': analysis['task_name']}
-    if 'run' in model['input']:
-        kwargs['run'] = model['input']['run'][0]
+    for d in arg: # you can list as many input dicts as you want here
+        for key, value in d.items():
+            dd[key].add(value)
+    return dict(((k, list(v)) if len(v) > 1 else (k, list(v)[0])
+                 for k, v in dd.items()))
 
-    # Write out events
-    bundle_paths = writeout_events(analysis, predictor_events, files_dir)
-    logger.info(model['input'])
+
+def _build_analysis(analysis, predictor_events, bids_dir, run_id=None):
+    tmp_dir = Path(mkdtemp())
+
+    entities = [{}]
+    if run_id is not None:
+        # Get entities of runs, and add to kwargs
+        for rid in run_id:
+            for run in analysis['runs']:
+                if rid == run['id']:
+                    entities.append(_get_entities(run))
+                    break
+
+    entities = _merge_dictionaries(*entities)
+    entities['scan_length'] = max([r['duration'] for r in analysis['runs']])
+    entities['task'] = analysis['task_name']
+
+    # Write out all events
+    paths = _writeout_events(analysis, predictor_events, tmp_dir)
     # Load events and try applying transformations
-    bids_layout = BIDSLayout([(bids_dir, 'bids'), (files_dir.as_posix(), 'derivatives')])
-    bids_analysis = Analysis(bids_layout, deepcopy(model))
-    bids_analysis.setup(**kwargs)
 
-    # Sidecar:
+    bids_layout = BIDSLayout(
+        [(bids_dir, 'bids'), (str(tmp_dir), ['bids', 'derivatives'])])
+    bids_analysis = Analysis(
+        bids_layout, deepcopy(analysis.get('model')))
+    bids_analysis.setup(**entities)
+
+    return tmp_dir, paths, bids_analysis
+
+@celery_app.task(name='workflow.compile')
+def compile(analysis, predictor_events, resources, bids_dir, run_ids):
+    tmp_dir, bundle_paths, bids_analysis = _build_analysis(
+        analysis, predictor_events, bids_dir, run_ids)
+
+
     sidecar = {"RepetitionTime": analysis['TR']}
+    # Write out JSON files
+    for obj, name in [
+        (analysis, 'analysis'),
+        (resources, 'resources'),
+        (analysis.get('model'), 'model'),
+        (sidecar, 'task-{}_bold'.format(analysis['task_name']))]:
 
-    # Write out analysis & resource JSON
-    for obj, name in [(analysis, 'analysis'), (resources, 'resources'),
-                      (model, 'model'), (sidecar, 'task-{}_bold'.format(analysis['task_name']))]:
-        path = (files_dir / name).with_suffix('.json')
+        path = (tmp_dir / name).with_suffix('.json')
         json.dump(obj, path.open('w'))
         bundle_paths.append((path.as_posix(), path.name))
 
@@ -87,3 +125,15 @@ def compile(analysis, predictor_events, resources, bids_dir):
             tar.add(path, arcname=arcname)
 
     return {'bundle_path': bundle_path}
+
+@celery_app.task(name='workflow.generate_dm')
+def generate_dm(analysis, predictor_events, bids_dir, run_ids):
+    _, _, bids_analysis = _build_analysis(
+        analysis, predictor_events, bids_dir, run_ids)
+
+    ## Get design matrix
+
+    ## Plot and save
+    dm_path = 'some_path'
+
+    return {'dm_path': dm_path}
--- neuroscout/resources/analysis/reports.py
+++ neuroscout/resources/analysis/reports.py
@@ -1,9 +1,10 @@
-from flask_apispec import doc
+from flask_apispec import doc, use_kwargs
 from database import db
 from sqlalchemy.dialects import postgresql
 from models import PredictorEvent
 from worker import celery_app
-
+import webargs as wa
+from marshmallow import fields
 from .endpoints import AnalysisMethodResource
 from ..utils import owner_required
 from .schemas import AnalysisFullSchema, AnalysisResourcesSchema
@@ -11,8 +12,7 @@ from .schemas import AnalysisFullSchema, AnalysisResourcesSchema
 
 def dump_pe(pes):
 	""" Custom function to serialize PredictorEvent, build for *SPEED*
-	Uses Core SQL queries. Warning: relies on attributes being in correct
-	order. """
+	Uses Core SQL queries. Relies on attributes being in correct order. """
 	statement = str(pes.statement.compile(dialect=postgresql.dialect()))
 	params = pes.statement.compile(dialect=postgresql.dialect()).params
 	res = db.session.connection().execute(statement, params)
@@ -25,43 +25,45 @@ def dump_pe(pes):
 	  } for r in res]
 
 def json_pes(analysis_json, run_id=None):
-	"""" Serialize to JSON and analysis's predictor events
-	This requires querying the PredictorEvents to get all events for all runs
-	and predictors.
-    """
+	"""" Serialize to JSON analysis's predictor events
+	Queries PredictorEvents to get all events for all runs and predictors. """
 	pred_ids = [p['id'] for p in analysis_json['predictors']]
-	run_ids = [r['id'] for r in analysis_json['runs']]
+	all_runs = [r['id'] for r in analysis_json['runs']]
 
-	if run_id is not None:
-		if run_id not in run_ids:
-			raise ValueError("Incorrect run id specified")
-		run_ids = [run_id]
+	if run_id is None:
+		run_id = all_runs
+	if not set(run_id) <= set(all_runs):
+		raise ValueError("Incorrect run id specified")
 
 	pes = PredictorEvent.query.filter(
 	    (PredictorEvent.predictor_id.in_(pred_ids)) & \
-	    (PredictorEvent.run_id.in_(run_ids)))
-	pes_json = dump_pe(pes)
+	    (PredictorEvent.run_id.in_(run_id)))
 
-	return pes_json
+	return dump_pe(pes)
 
 class CompileAnalysisResource(AnalysisMethodResource):
-    @doc(summary='Compile and lock analysis.')
-    @owner_required
-    def post(self, analysis):
-        analysis.status = 'SUBMITTING'
-        analysis.compile_traceback = ''
-        db.session.add(analysis)
-        db.session.commit()
+	@doc(summary='Compile and lock analysis.')
+	@use_kwargs({
+		'run_id': wa.fields.DelimitedList(fields.Int(),
+	                                    description='Run id(s).')
+	}, locations=['query'])
+	@owner_required
+	def post(self, analysis, run_id=None):
+		analysis.status = 'SUBMITTING'
+		analysis.compile_traceback = ''
+		db.session.add(analysis)
+		db.session.commit()
 
-        analysis_json = AnalysisFullSchema().dump(analysis)[0]
-        resources_json = AnalysisResourcesSchema().dump(analysis)[0]
-        pes_json = json_pes(analysis_json)
-        task = celery_app.send_task('workflow.compile',
-        		args=[analysis_json, pes_json, resources_json,
-        		analysis.dataset.local_path])
-        analysis.compile_task_id = task.id
-        analysis.status = 'PENDING'
-        db.session.add(analysis)
-        db.session.commit()
+		analysis_json = AnalysisFullSchema().dump(analysis)[0]
+		resources_json = AnalysisResourcesSchema().dump(analysis)[0]
+		pes_json = json_pes(analysis_json) # Send PEs for all runs
 
-        return analysis
+		task = celery_app.send_task('workflow.compile',
+				args=[analysis_json, pes_json, resources_json,
+				analysis.dataset.local_path, run_id])
+		analysis.compile_task_id = task.id
+		analysis.status = 'PENDING'
+		db.session.add(analysis)
+		db.session.commit()
+
+		return analysis
