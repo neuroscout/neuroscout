5c5fc72e3e6cf3b62c5130a44f6171b3df1add88
adelavega <delavega@utexas.edu>
2018-10-23 20:14:55 -0500

    Added space between analysis ID and save button

--- neuroscout/frontend/src/Builder.tsx
+++ neuroscout/frontend/src/Builder.tsx
@@ -120,7 +120,7 @@ const normalizeDataset = (d: ApiDataset): Dataset => {
 
 // Get list of tasks from a given dataset
 export const getTasks = (datasets: Dataset[], datasetId: string | null): Task[] => {
-    let curDataset = datasets.find((x) => { 
+    let curDataset = datasets.find((x) => {
       return x.id === datasetId;
     });
 
@@ -593,7 +593,7 @@ export default class AnalysisBuilder extends React.Component<BuilderProps & Rout
   }
 
   runIdsFromModel = (availableRuns: Run[], input: ImageInput) => {
-    let runIds: Run[] = availableRuns; 
+    let runIds: Run[] = availableRuns;
     if (!this.state.model || !this.state.model.input) {
       return [];
     }
@@ -649,7 +649,7 @@ export default class AnalysisBuilder extends React.Component<BuilderProps & Rout
                 predictorsActive: true
               });
             }
-            
+
             this.setState({
               availableRuns: data,
               availablePredictors: [],
@@ -790,6 +790,7 @@ export default class AnalysisBuilder extends React.Component<BuilderProps & Rout
             <h2>
               {!isDraft ? <Icon type="lock" /> : <Icon type="unlock" />}
               {`Analysis ID: ${analysis.analysisId || 'n/a'}`}
+              <Space />
               <Space />
               <Button
                 onClick={this.saveAnalysis({ compile: false })}
