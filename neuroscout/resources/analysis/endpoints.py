17f664cb9dcf3b594a42fa4230531796db009cc3
delavega4 <aleph4@gmail.com>
2019-01-03 17:46:50 -0600

    PEP8 Flake

--- neuroscout/resources/analysis/endpoints.py
+++ neuroscout/resources/analysis/endpoints.py
@@ -9,97 +9,104 @@ import datetime
 from utils.db import put_record
 from ..utils import owner_required, auth_required, fetch_analysis, abort
 from .schemas import (AnalysisSchema, AnalysisFullSchema,
-					 AnalysisResourcesSchema)
+                      AnalysisResourcesSchema)
+
 
 @doc(tags=['analysis'])
 @marshal_with(AnalysisSchema)
 class AnalysisMethodResource(MethodResource):
-	pass
+    pass
+
 
 class AnalysisRootResource(AnalysisMethodResource):
-	"""" Resource for root address """
-	@marshal_with(AnalysisSchema(many=True))
-	@doc(summary='Returns list of public analyses.')
-	def get(self):
-		return Analysis.query.filter_by(private=False, status='PASSED').all()
+    """" Resource for root address """
+    @marshal_with(AnalysisSchema(many=True))
+    @doc(summary='Returns list of public analyses.')
+    def get(self):
+        return Analysis.query.filter_by(private=False, status='PASSED').all()
 
-	@use_kwargs(AnalysisSchema)
-	@doc(summary='Add new analysis!')
-	@auth_required
-	def post(self, **kwargs):
-		new = Analysis(user_id = current_identity.id, **kwargs)
-		db.session.add(new)
-		db.session.commit()
-		return new
+    @use_kwargs(AnalysisSchema)
+    @doc(summary='Add new analysis!')
+    @auth_required
+    def post(self, **kwargs):
+        new = Analysis(user_id=current_identity.id, **kwargs)
+        db.session.add(new)
+        db.session.commit()
+        return new
+
 
 class AnalysisResource(AnalysisMethodResource):
-	@doc(summary='Get analysis by id.')
-	@fetch_analysis
-	def get(self, analysis):
-		return analysis
+    @doc(summary='Get analysis by id.')
+    @fetch_analysis
+    def get(self, analysis):
+        return analysis
 
-	@doc(summary='Edit analysis.')
-	@use_kwargs(AnalysisSchema)
-	@owner_required
-	def put(self, analysis, **kwargs):
-		if analysis.locked == True:
-			abort(422, "Analysis is locked. It cannot be edited.")
-		if analysis.status not in ['DRAFT', 'FAILED']:
-			exceptions = ['private', 'description']
-			kwargs = {k: v for k, v in kwargs.items() if k in exceptions}
-			if not kwargs:
-				abort(422, "Analysis is not editable. Try cloning it.")
-		kwargs['modified_at'] = datetime.datetime.utcnow()
-		return put_record(kwargs, analysis)
+    @doc(summary='Edit analysis.')
+    @use_kwargs(AnalysisSchema)
+    @owner_required
+    def put(self, analysis, **kwargs):
+        if analysis.locked is True:
+                abort(422, "Analysis is locked. It cannot be edited.")
+                if analysis.status not in ['DRAFT', 'FAILED']:
+                    exceptions = ['private', 'description']
+                    kwargs = {k: v for k, v in kwargs.items() if k in exceptions}
+        if not kwargs:
+            abort(422, "Analysis is not editable. Try cloning it.")
+        kwargs['modified_at'] = datetime.datetime.utcnow()
+        return put_record(kwargs, analysis)
 
-	@doc(summary='Delete analysis.')
-	@owner_required
-	def delete(self, analysis):
-		if analysis.status not in ['DRAFT', 'FAILED'] or analysis.locked == True:
-			abort(422, "Analysis not editable, cannot delete.")
-		db.session.delete(analysis)
-		db.session.commit()
+    @doc(summary='Delete analysis.')
+    @owner_required
+    def delete(self, analysis):
+        if analysis.status not in ['DRAFT', 'FAILED'] or analysis.locked is True:
+                abort(422, "Analysis not editable, cannot delete.")
+        db.session.delete(analysis)
+        db.session.commit()
 
-		return {'message' : 'deleted!'}
+        return {'message': 'deleted!'}
+
 
 class CloneAnalysisResource(AnalysisMethodResource):
-	@doc(summary='Clone analysis.')
-	@auth_required
-	@fetch_analysis
-	def post(self, analysis):
-		if analysis.user_id != current_identity.id:
-			if analysis.status != 'PASSED':
-				abort(422, "You can only clone somebody else's analysis"
-								  " if they have been compiled.")
+    @doc(summary='Clone analysis.')
+    @auth_required
+    @fetch_analysis
+    def post(self, analysis):
+        if analysis.user_id != current_identity.id:
+            if analysis.status != 'PASSED':
+                abort(422, "You can only clone somebody else's analysis"
+                      " if they have been compiled.")
 
-		cloned = analysis.clone(current_identity)
-		db.session.add(cloned)
-		db.session.commit()
-		return cloned
+        cloned = analysis.clone(current_identity)
+        db.session.add(cloned)
+        db.session.commit()
+        return cloned
+
 
 class AnalysisFullResource(AnalysisMethodResource):
-	@marshal_with(AnalysisFullSchema)
-	@doc(summary='Get analysis (including nested fields).')
-	@fetch_analysis
-	def get(self, analysis):
-		return analysis
+    @marshal_with(AnalysisFullSchema)
+    @doc(summary='Get analysis (including nested fields).')
+    @fetch_analysis
+    def get(self, analysis):
+        return analysis
+
 
 class AnalysisResourcesResource(AnalysisMethodResource):
-	@marshal_with(AnalysisResourcesSchema)
-	@doc(summary='Get analysis resources.')
-	@fetch_analysis
-	def get(self, analysis):
-		return analysis
+    @marshal_with(AnalysisResourcesSchema)
+    @doc(summary='Get analysis resources.')
+    @fetch_analysis
+    def get(self, analysis):
+        return analysis
+
 
 class AnalysisBundleResource(MethodResource):
-	@doc(tags=['analysis'], summary='Get analysis tarball bundle.',
-	responses={"200": {
-		"description": "gzip tarball, including analysis, resources and events.",
-		"type": "application/x-tar"}})
-	@fetch_analysis
-	def get(self, analysis):
-		if (analysis.status != "PASSED" or analysis.bundle_path is None or \
-		not exists(analysis.bundle_path)):
-			msg = "Analysis bundle not available. Try compiling."
-			abort(404, msg)
-		return send_file(analysis.bundle_path, as_attachment=True)
+    @doc(tags=['analysis'], summary='Get analysis tarball bundle.',
+         responses={"200": {
+             "description": "gzip tarball, including analysis, resources and events.",
+             "type": "application/x-tar"}})
+    @fetch_analysis
+    def get(self, analysis):
+        if (analysis.status != "PASSED" or analysis.bundle_path is None or
+           not exists(analysis.bundle_path)):
+            msg = "Analysis bundle not available. Try compiling."
+            abort(404, msg)
+        return send_file(analysis.bundle_path, as_attachment=True)
