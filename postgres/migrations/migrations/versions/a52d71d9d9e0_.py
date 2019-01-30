c7a2ac6b638323b27e69d0365b5af184bdf84b27
Alejandro de la Vega <aleph4@gmail.com>
2019-01-30 11:19:36 -0600

    Add migrations

---
+++ postgres/migrations/migrations/versions/a52d71d9d9e0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a52d71d9d9e0
+Revises: f991ae1b4c35, 26e8ef347559
+Create Date: 2019-01-30 17:18:20.377581
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a52d71d9d9e0'
+down_revision = ('f991ae1b4c35', '26e8ef347559')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
