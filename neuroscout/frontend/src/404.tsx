5a1dc210a34f7fd33c9936ddefc095841ba25ae2
Ross Blair <rblair2@stanford.edu>
2019-02-04 16:47:39 -0800

    have 404 page be location aware to display attempted url and to offer to go back

--- neuroscout/frontend/src/404.tsx
+++ neuroscout/frontend/src/404.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import { Divider, Row } from 'antd';
 import { MainCol } from './HelperComponents';
+import { createBrowserHistory } from 'history';
 
 export default class NotFound extends React.Component<{}, {}> {
 
   render() {
+    const history = createBrowserHistory();
     return (
       <Row type="flex" justify="center" style={{padding: 0 }}>
         <MainCol>
-          <Divider>Page not found</Divider>
+          <Divider>Not found</Divider>
+          The requested URL {history.location.pathname} was not found.
+          Go <a onClick={history.goBack}>back</a>?
         </MainCol>
       </Row>
     );
