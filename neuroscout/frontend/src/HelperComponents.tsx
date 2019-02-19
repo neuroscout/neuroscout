d365d4f3729e9063c47991086a0e8272446a4ee3
Ross Blair <rblair2@stanford.edu>
2019-02-19 10:38:12 -0800

    make component for displaying error message instead of duplicating code.

--- neuroscout/frontend/src/HelperComponents.tsx
+++ neuroscout/frontend/src/HelperComponents.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Col } from 'antd';
+import { Alert, Col } from 'antd';
 
 // Simple space component to seperate buttons, etc.
 // tslint:disable-next-line:jsx-self-close
@@ -11,6 +11,33 @@ export class MainCol extends React.Component<{}, {}> {
       <Col xxl={{span: 16}} xl={{span: 18}} lg={{span: 20}} xs={{span: 24}} className="mainCol">
         {this.props.children}
       </Col>
+    );
+  }
+}
+
+export class DisplayErrorsInline extends React.Component<{errors: string[]}, {}> {
+  render() {
+    return (
+      <>
+        {this.props.errors.length > 0 &&
+          <div>
+            <Alert
+              type="error"
+              showIcon={true}
+              closable={true}
+              message={
+                <ul>
+                  {this.props.errors.map((x, i) =>
+                    <li key={i}>
+                      {x}
+                    </li>
+                  )}
+                </ul>
+              }
+            />
+            <br />
+          </div>}
+      </>
     );
   }
 }
