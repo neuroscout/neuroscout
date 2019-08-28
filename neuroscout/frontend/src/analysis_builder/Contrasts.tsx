467101c46a72f1e7cca9c2d0edcf0ebd344bea8c
Ross Blair <rosswilsonblair@gmail.com>
2019-08-28 10:27:16 -0500

    for auto contrasts only add the selected predictor and a single weight fixes #509

--- neuroscout/frontend/src/analysis_builder/Contrasts.tsx
+++ neuroscout/frontend/src/analysis_builder/Contrasts.tsx
@@ -147,6 +147,8 @@ export class ContrastsTab extends React.Component<ContrastsTabProps, ContrastsTa
       newContrast.Name = `${x.name}`;
       newContrast.ConditionList = predictors.map(y => y.name);
       newContrast.Weights = predictors.map(y => y.name === x.name ? 1 : 0);
+      newContrast.ConditionList = [ x.name ];
+      newContrast.Weights = [ 1 ];
       newContrasts.push(newContrast);
     });
     this.props.onSave(newContrasts);
