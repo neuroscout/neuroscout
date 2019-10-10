d9fce9f45673ea6946cb48d032945742efc39f02
Ross Blair <rosswilsonblair@gmail.com>
2019-10-10 10:40:40 -0500

    add basic sentry configuration

--- neuroscout/frontend/src/index.tsx
+++ neuroscout/frontend/src/index.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
+
+Sentry.init({dsn: 'https://779d1809d42f48fe8a58a69eece1c9f6@sentry.io/1775913'});
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
