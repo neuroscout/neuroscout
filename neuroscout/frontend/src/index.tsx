4f6e165a1a8fff160c24ece96c7624ee613667c3
Alejandro de la Vega <aleph4@gmail.com>
2019-10-10 16:25:45 -0500

    Update sentry url for frontend

--- neuroscout/frontend/src/index.tsx
+++ neuroscout/frontend/src/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/browser';
 import App from './App';
 
-Sentry.init({dsn: 'https://779d1809d42f48fe8a58a69eece1c9f6@sentry.io/1775913'});
+Sentry.init({dsn: 'https://96e0d2454fea493e886b3122504e22d3@sentry.io/1776214'});
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
