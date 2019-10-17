75c2b0c03674e566911912f06686de95fbad7282
Alejandro de la Vega <aleph4@gmail.com>
2019-10-17 11:32:07 -0500

    Add sentry to celery

--- celery_worker/app.py
+++ celery_worker/app.py
@@ -1,5 +1,9 @@
 from celery import Celery
 from neuroscout.basic import create_app
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
+
+sentry_sdk.init("https://8d72e53e214c4ae1af7e34fa776659d8@sentry.io/1783101", integrations=[CeleryIntegration()])
 
 celery_app = Celery('tasks')
 
