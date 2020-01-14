aa14b85ec52f63ec6f9d9cc41c4f50f8be7f8022
Ross Blair <rosswilsonblair@gmail.com>
2020-01-14 10:19:10 -0600

    was supposed to be download link for design not correlation matrix...

--- neuroscout/frontend/src/analysis_builder/Report.tsx
+++ neuroscout/frontend/src/analysis_builder/Report.tsx
@@ -66,12 +66,14 @@ class Plots extends React.Component<{plots: any[], corr_plots: any[], runTitles:
           <TabPane tab={this.props.runTitles[i]} key={'' + i}>
             <Collapse bordered={false} defaultActiveKey={['dm']}>
              <Panel header="Design Matrix" key="dm">
-              <VegaPlot spec={this.props.plots[i]}/>
+               <VegaPlot
+                 spec={this.props.plots[i]}
+                 title={this.props.runTitles[i].replace(/ /g, '').concat('.png')}
+               />
              </Panel>
              <Panel header="Correlation Matrix" key="cm">
                <VegaPlot
                  spec={this.props.corr_plots[i]}
-                 title={this.props.runTitles[i].replace(/ /g, '').concat('.png')}
                />
              </Panel>
             </Collapse>
