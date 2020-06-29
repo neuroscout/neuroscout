fa3e968776ce826513a9cd771ede16075ee2dd13
delavega4 <aleph4@gmail.com>
2020-06-29 16:52:41 -0500

    Add institution field

--- neuroscout/models/auth.py
+++ neuroscout/models/auth.py
@@ -15,6 +15,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(255))
     picture = db.Column(db.Text)
+    institution = db.Columns(db.Text)
 
     name = db.Column(db.String(40))
     active = db.Column(db.Boolean())  # If set to disabled, cannot access.
