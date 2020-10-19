a2a85acb9a77ab245a728680c5f1080e9412bd0a
delavega4 <aleph4@gmail.com>
2020-10-19 13:56:08 -0500

    Get run_id separately in tests

--- neuroscout/tests/api/test_predictor.py
+++ neuroscout/tests/api/test_predictor.py
@@ -34,7 +34,9 @@ def test_get_predictor(auth_client, extract_features):
     ds = decode_json(
         auth_client.get('/api/datasets'))
     dataset_id = ds[0]['id']
-    run_id = str(ds[0]['runs'][0])
+
+    run_id = auth_client.get('/api/runs', params={'dataset_id': dataset_id})
+    run_id = [r['id'] for r in run_id]
 
     resp = auth_client.get('/api/predictors', params={'run_id': run_id})
     assert resp.status_code == 200
