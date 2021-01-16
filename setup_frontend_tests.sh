60adf17fe84ab65aea2994055ed5db923e972612
delavega4 <aleph4@gmail.com>
2021-01-15 18:47:42 -0600

    Fix test setup

--- setup_frontend_tests.sh
+++ setup_frontend_tests.sh
@@ -1,7 +1,6 @@
 python manage.py setup_test_db
-/usr/local/bin/gunicorn -b :4000 neuroscout.core:app --log-level debug --timeout 120 &
+gunicorn -b :4000 neuroscout.core:app --log-level debug --timeout 120 &
 cd neuroscout/frontend
-mv src/config.ts src/config.ts.bak
 cp src/config.ts.example src/config.ts
 wait-on http://0.0.0.0:4000/api/datasets
 yarn start&
