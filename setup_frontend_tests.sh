8d1b219619e392ae131db07ba551a9ec160351e4
delavega4 <aleph4@gmail.com>
2021-01-15 19:03:30 -0600

    Move cypress JSON file

--- setup_frontend_tests.sh
+++ setup_frontend_tests.sh
@@ -1,6 +1,7 @@
 python manage.py setup_test_db
 gunicorn -b :4000 neuroscout.core:app --log-level debug --timeout 120 &
 cd neuroscout/frontend
+cp cypress-test.json cypress.json
 cp src/config.ts.example src/config.ts
 wait-on http://0.0.0.0:4000/api/datasets
 yarn start&
