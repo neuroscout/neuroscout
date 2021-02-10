374fa0060488192a91b638deae17bd1e43169aad
delavega4 <aleph4@gmail.com>
2021-02-10 12:38:09 -0600

    Fix test get ext test

--- neuroscout/tests/api/test_extractor.py
+++ neuroscout/tests/api/test_extractor.py
@@ -1,19 +1,19 @@
 from ..request_utils import decode_json
 
 
-def test_get_dataset(auth_client, add_local_task_json):
+def test_extractor_desc(auth_client, add_local_task_json):
     # List of datasets
     resp = auth_client.get('/api/extractors')
     assert resp.status_code == 200
-    dataset_list = decode_json(resp)
-    assert type(dataset_list) == list
+    ext_list = decode_json(resp)
+    assert type(ext_list) == list
 
     # Get first dataset
-    assert 'description' in dataset_list[0]
-    assert 'name' in dataset_list[0]
+    assert 'description' in ext_list[0]
+    assert 'name' in ext_list[0]
 
-    assert len(dataset_list) > 20
+    assert len(ext_list) > 2
 
-    ext = [ext for ext in dataset_list if ext['name'] == 'RMSExtractor'][0]
+    ext = [ext for ext in ext_list if ext['name'] == 'RMSExtractor'][0]
 
     assert 'Extracts root mean square (RMS) from audio' in ext['description']
