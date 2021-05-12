932ce091a6a4945a25fb936d121c9ed61a46a05d
Alejandro de la Vega <aleph4@gmail.com>
2021-05-12 10:47:38 -0500

    Add PGPASSWORD to sql dump script

--- postgres/pg_dump-to-s3/pg_dump-to-s3.sh
+++ postgres/pg_dump-to-s3/pg_dump-to-s3.sh
@@ -11,6 +11,7 @@ PG_USER="postgres"
 # S3
 S3_PATH="neuroscout-backup"
 
+
 # get databases list
 dbs=("$@")
 
@@ -20,7 +21,7 @@ DIR=/home
 
 for db in "${dbs[@]}"; do
     # Dump database
-    pg_dump -Fc -h $PG_HOST -U $PG_USER $db > /tmp/"$NOW"_"$db".dump
+    PGPASSWORD=$POSTGRES_PASSWORD pg_dump -Fc -h $PG_HOST -U $PG_USER $db > /tmp/"$NOW"_"$db".dump
 
     # Copy to S3
     aws s3 cp /tmp/"$NOW"_"$db".dump s3://$S3_PATH/"$NOW"_"$db".dump --storage-class STANDARD_IA
