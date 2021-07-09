ef96116aa33bca700e13c947355c34d27b8e30c5
Alejandro de la Vega <aleph4@gmail.com>
2021-07-09 18:11:43 -0500

    Fix tests

--- neuroscout/tests/test_models.py
+++ neuroscout/tests/test_models.py
@@ -60,10 +60,6 @@ def test_dataset_ingestion(session, add_task):
     assert GroupPredictor.query.count() == 3
     assert GroupPredictor.query.filter_by(name='sex').count() == 1
 
-    gpv = GroupPredictor.query.filter_by(name='sex').one().values
-    assert len(gpv) == 4
-    assert 'F' in [v.value for v in gpv]
-
 
 def test_json_local_dataset(session, add_local_task_json):
     dataset_model = Dataset.query.filter_by(id=add_local_task_json).one()
