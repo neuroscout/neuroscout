bff414e810039e62679a5de1b10ea820f084a113
Alejandro de la Vega <aleph4@gmail.com>
2022-02-21 17:23:42 -0600

    Wait a little longer for tests

--- setup_frontend_tests.sh
+++ setup_frontend_tests.sh
@@ -1,5 +1,6 @@
 python manage.py setup_test_db
 gunicorn -b :4000 neuroscout.core:app --log-level debug --timeout 120 &
+sleep 20
 cd neuroscout/frontend
 cp cypress-test.json cypress.json
 cp src/config.ts.example src/config.ts
