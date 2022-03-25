921c694d5a2a296fbbcec0c456cb1dd6c81b462f
Ross Blair <rosswilsonblair@gmail.com>
2022-03-25 16:15:51 -0500

    undo padding change

--- neuroscout/frontend/src/Navbar.tsx
+++ neuroscout/frontend/src/Navbar.tsx
@@ -21,10 +21,7 @@ class Navbar extends React.Component<UserStore, Record<string, never>> {
             style={{ lineHeight: '64px', justifyContent: 'flex-end' }}
             selectedKeys={[]}
           >
-            <Menu.Item
-              style={{ marginRight: 'auto', paddingLeft: '0px' }}
-              key="home"
-            >
+            <Menu.Item style={{ marginRight: 'auto' }} key="home">
               <Link to="/" style={{ fontSize: 20 }}>
                 Neuroscout
               </Link>
