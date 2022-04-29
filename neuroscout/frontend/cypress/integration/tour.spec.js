f55c85bf7df3393100d40cb0ff52a32d31d15e00
Ross Blair <rosswilsonblair@gmail.com>
2022-04-29 13:14:14 -0500

    access button directly by id in cypress

--- neuroscout/frontend/cypress/integration/tour.spec.js
+++ neuroscout/frontend/cypress/integration/tour.spec.js
@@ -3,6 +3,6 @@ describe('Tour', () => {
     cy.visit('/')
     cy.login('user@example.com', 'string')
     cy.wait(200)
-    cy.get('.ant-btn').contains('Tour!').click()
+    cy.get('#rainbow-btn').click()
   })
 })
