f54c626be8d1e9ed139ebba528effd890774b540
Ross Blair <rosswilsonblair@gmail.com>
2022-04-29 12:53:52 -0500

    bump wait after login for tour test, see if failure is login slowness in ci

--- neuroscout/frontend/cypress/integration/tour.spec.js
+++ neuroscout/frontend/cypress/integration/tour.spec.js
@@ -2,7 +2,7 @@ describe('Tour', () => {
   it('opens tour', () => {
     cy.visit('/')
     cy.login('user@example.com', 'string')
-    cy.wait(100)
-    cy.get('.ant-btn-primary').contains('Tour!').click()
+    cy.wait(200)
+    cy.get('.ant-btn').contains('Tour!').click()
   })
 })
