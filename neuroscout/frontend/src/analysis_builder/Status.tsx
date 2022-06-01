e35c78d0d1c020e4b7c740d27a9c57751f15624d
Ross Blair <rosswilsonblair@gmail.com>
2022-06-01 12:43:33 -0500

    add collab link in run tab

--- neuroscout/frontend/src/analysis_builder/Status.tsx
+++ neuroscout/frontend/src/analysis_builder/Status.tsx
@@ -252,6 +252,16 @@ export class StatusTab extends React.Component<submitProps, statusTabState> {
               <span> </span>
             </>
           )}
+
+          {this.props.status === 'PASSED' && (
+            <a href="https://colab.research.google.com/github/neuroscout/neuroscout-cli/blob/master/examples/Neuroscout_Colab_Demo_NoMount.ipynb">
+              <img
+                data-canonical-src="https://colab.research.google.com/assets/colab-badge.svg"
+                alt="Open In Colab"
+                src="https://camo.githubusercontent.com/84f0493939e0c4de4e6dbe113251b4bfb5353e57134ffd9fcab6b8714514d4d1/68747470733a2f2f636f6c61622e72657365617263682e676f6f676c652e636f6d2f6173736574732f636f6c61622d62616467652e737667"
+              />
+            </a>
+          )}
           <DateTag
             modified_at={this.props.modified_at ? this.props.modified_at : ''}
           />
