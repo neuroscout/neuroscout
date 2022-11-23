509743886474cdb55a8802e2a14c5c7c6813c1f3
Alejandro de la Vega <aleph4@gmail.com>
2022-11-23 16:21:40 -0600

    Look for manage in neuroscout/

--- setup_frontend_tests.sh
+++ setup_frontend_tests.sh
@@ -1,6 +1,7 @@
+cd neuroscout/
 python manage.py setup_test_db
 gunicorn -b :4000 neuroscout.core:app --log-level debug --timeout 120 &
-cd neuroscout/frontend
+cd frontend
 cp cypress-test.json cypress.json
 cp src/config.ts.example src/config.ts
 echo "Starting web server"
