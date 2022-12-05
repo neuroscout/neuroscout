34198e2e457f7ad7cbb108d62aff3718a378a04e
Alejandro de la Vega <aleph4@gmail.com>
2022-12-05 12:27:58 -0600

    Move back up dir

--- setup_frontend_tests.sh
+++ setup_frontend_tests.sh
@@ -1,7 +1,8 @@
 cd neuroscout/
 python manage.py setup_test_db
+cd ../
 gunicorn -b :4000 neuroscout.core:app --log-level debug --timeout 120 &
-cd frontend
+cd neuroscout/frontend
 cp cypress-test.json cypress.json
 cp src/config.ts.example src/config.ts
 echo "Starting web server"
